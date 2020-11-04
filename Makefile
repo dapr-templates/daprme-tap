5894fb6c91252bef4c4ee9148769c2a51a00ff59
Mark Chmarny <mark@chmarny.com>
2020-11-04 11:21:26 -0800

    manual sha update

--- Makefile
+++ Makefile
@@ -7,6 +7,10 @@ tag: ## Creates release tag
 	git push origin $(RELEASE_TAG)
 .PHONY: tag
 
+tag-deleted:
+	git tag -d $(RELEASE_TAG)
+.PHONY: tag-deleted
+
 help: ## Display available commands
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk \
 		'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- daprme.rb
+++ daprme.rb
@@ -1,8 +1,8 @@
 class Daprme < Formula
   desc "New app wizard for creation of a new Dapr project"
   homepage "https://dapr.io"
-  url "https://github.com/dapr-templates/daprme/releases/download/v0.6.1/daprme"
-  sha256 "03493a00a6ca29b60cd03be415858da8dcc00c5567d9f89c6c666716f4ae20f4"
+  url "https://github.com/dapr-templates/daprme/releases/download/v0.6.2/daprme"
+  sha256 "6fa9b8372845e96ca70a03f9d53480b3917ccf3ab8ea1ba15fbeed681906466a"
   license "MIT"
 
   def install
