7a1688933066a74e269f5f777f7732b60cd95be5
Mark Chmarny <mchmarny@users.noreply.github.com>
2020-11-05 06:46:45 -0800

    sha fix

--- daprme.rb
+++ daprme.rb
@@ -2,7 +2,7 @@ class Daprme < Formula
   desc "New app wizard for creation of a new Dapr project"
   homepage "https://dapr.io"
   url "https://github.com/dapr-templates/daprme/releases/download/v0.6.3/daprme"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855 "
+  sha256 "59ee8130cfd3dd3fd93f9210ce5e3a90a1a9c70e88ac7a6e4c2e11e41ae91d9c"
   license "MIT"
 
   def install
